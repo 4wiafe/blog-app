6507a26d2e2495f5ca4fb1e7b79cd363cf7c0604
Richmond Wiafe Gyebi <gyebirichmond21@gmail.com>
2026-09-25 18:05:10 +0000

    Add end point to fetch logged in active user

--- backend/src/routers/user_route.py
+++ backend/src/routers/user_route.py
@@ -1,18 +1,27 @@
-from fastapi import APIRouter, status
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, status
+
+from models.user import User
 from schemas.user_schemas import UserCreate, UserPublic
-from services.user_service import register_user
+from services.user_service import get_current_active_user, register_user
 from database import SessionDep
 
-router = APIRouter()
+router = APIRouter(tags=["users"])
 
 
 @router.post(
     "/register",
-    tags=["users"],
     response_model=UserPublic,
     status_code=status.HTTP_201_CREATED,
 )
 def add_new_user(user: UserCreate, session: SessionDep):
     registered_user = register_user(user, session)
     return registered_user
+
+
+@router.get("/users/me", response_model=UserPublic)
+def read_active_user(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
